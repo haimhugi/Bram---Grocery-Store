4e5c2a43ee44e3385071951e635273eb7c4d795b
Yaakov Dayan <yaakovdayan1@gmail.com>
2021-05-23 15:48:29 +0300

    Remove 'Desc' field

--- Bram - grocery store/Models/Product.cs
+++ Bram - grocery store/Models/Product.cs
@@ -21,8 +21,6 @@ namespace Bram___grocery_store.Models
         [Display(Name = "Photo Product Url: ")]
         public string PhotoUrl { get; set; }
 
-        public string Desc { get; set; }
-
         [Display(Name = "Choose Category: ")]
         public int CategoryId { get; set; }
         public Category Category { get; set; }
